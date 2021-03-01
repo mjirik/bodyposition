160a3f125f1778249c34b36811a1399afa03aea3
vjavorek <javorek.v@gmail.com>
2021-03-01 17:11:26 +0100

    core class

---
+++ bodyposition/bodyposition.py
@@ -0,0 +1,18 @@
+from loguru import logger
+import numpy as np
+
+class BodyPosition(
+    
+):
+    def __init__(
+        self, imshape, head_first=True
+        ):
+        
+        def get_dist_to_sagittal():
+            sdf_type = 'sagittal'
+        
+        def get_dist_to_coronal():
+            sdf_type = 'coronal'
+        
+        def get_dist_to_surface():
+            sdf_type = 'surface'
