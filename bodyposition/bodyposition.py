4ceb0c70f581ed2a28523522d277c8c53c36dfb5
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-03-04 08:34:56 +0100

    tests

--- bodyposition/bodyposition.py
+++ bodyposition/bodyposition.py
@@ -1,37 +1,49 @@
 from loguru import logger
 import numpy as np
+import imma.image
 
-class BodyPosition(
-    
-):
+models = {}
+
+class BodyPosition:
+
     def __init__(
-        self, imshape=256
-        ):
+        self, data3d, voxelsize_mm, imshape=256, ):
         self.imshape = 256
         self.sdf_type = ''
-        self.model = None
-        
-    def get_dist_to_sagittal(self, data):
+        self.models = models
+        self.working_vs = np.asarray([1.] * 3)
+        self.data3dr = imma.image.resize_to_mm(data3d, voxelsize_mm, self.working_vs)
+        self.orig_shape = data3d.shape
+
+    def get_dist_to_sagittal(self):
+        data = self.data3d
         sdf_type = 'sagittal'
-        if self.model = None:
-            model = get_model(sdf_type)
+        if self.model == None:
+            model = self._get_model(sdf_type)
             self.model = model
         predictions = model.predict(data)
         predictions = np.asarray(predictions).reshape(np.asarray(predictions).shape[0], self.imshape, self.imshape)
         self.predictions = predictions
-        return predictions
-    
+        return self._resize_to_orig_shape(predictions)
+
+    def _resize_to_orig_shape(self, data):
+        return imma.image.resize_to_shape(data, self.orig_shape)
+
     def get_dist_to_coronal(self, data):
         sdf_type = 'coronal'
-        model = get_model(sdf_type)
+        model = self._get_model(sdf_type)
     
     def get_dist_to_surface(self, data):
         sdf_type = 'surface'
-        model = get_model(sdf_type)
+        model = self._get_model(sdf_type)
     
-    def get_model(self, sdf_type):
+    def _get_model(self, sdf_type):
         from tensorflow.keras.models import load_model
-        model = load_model(f"sdf_unet_{sdf_type}.h5")
+        if sdf_type in models:
+            model = models[sdf_type]
+        else:
+            model = load_model(f"sdf_unet_{sdf_type}.h5")
+            models[sdf_type] = model
         return model
     
     def get_data(self, dataset, scannum):
--- tests/bodyposition_test.py
+++ tests/bodyposition_test.py
@@ -1,6 +1,23 @@
 import bodyposition
 import pytest
+import io3d
+import bodyposition.bodyposition as bpo
+import numpy as np
+
 
 def test_import():
-    pass
+
+    data3d_orig = io3d.read_dataset("3Dircadb1", "data3d", 1)
+    bp = bpo.BodyPosition(data3d_orig.data3d, data3d_orig.voxelsize_mm)
+    sdf_sagittal = bp.get_dist_to_sagittal()
+
+    assert np.max(sdf_sagittal) > 0
+    assert np.min(sdf_sagittal) < 0
+
+    imsize_px = np.prod(data3d_orig.data3d.shape)
+    imsize_px_010 = imsize_px * 0.1
+    assert np.sum(sdf_sagittal > 0) > imsize_px_010
+    assert np.sum(sdf_sagittal < 0) > imsize_px_010
     # bodyposition.nejkyvnitrnimodul.fcn1()
+
+
