ab630e11ccc22d91d8d47d692db74aa980983840
vjavorek <javorek.v@gmail.com>
2021-04-13 16:46:15 +0200

    tf downgrade + tensorboard
    tensorflow 2.4>2.1, python 3.9>3.7

--- devel/metacentrum/metadiaphragm_axial.py
+++ devel/metacentrum/metadiaphragm_axial.py
@@ -1,14 +1,14 @@
 import sys
 print(sys.path)
 
-from bodyposition import sdf_unet256
+from bodyposition import sdf_unet256, sdf_unet256_tensorboard
 import time
 
 sdf_type='diaphragm_axial'
 
 start_time = time.time()
 
-sdf_unet256.train(
+sdf_unet256_tensorboard.train(
     imshape=256,
     sdf_type=sdf_type,
     # sdf_type='coronal',
@@ -16,7 +16,7 @@ sdf_unet256.train(
     # sdf_type='surface',
     skip_h5=False,
     batch_size=16,
-    epochs=100,
+    epochs=200,
     filename_prefix='',
     validation_ids = [19, 39],
     test_ids = [20, 40],
